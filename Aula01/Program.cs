e64c295ba98f5a1a90b2eb3ed6a11fdadcec1edf
jonataferreira <ferreira.jonata@gmail.com>
2024-08-02 11:10:19 -0300

    Commit da Aula 02

--- Aula01/Program.cs
+++ Aula01/Program.cs
@@ -1,7 +1,19 @@
 ﻿using Aula01.Models;
 
-Pessoa p = new Pessoa();
+
+/*Pessoa p = new Pessoa();
 
 p.Nome = "Jonata";
 p.Idade = 31;
-p.Apresentar();
+p.Apresentar();*/
+string apresentacao = "Olá seja bem vindo";
+// double altura = 1.80;
+// decimal preco = 1.80M; //decimaal representa valores monetários
+
+DateTime dataAtual = DateTime.Now;
+DateTime data5Dias = DateTime.Now.AddDays(5);
+
+Console.WriteLine(apresentacao);   
+Console.WriteLine("São " + dataAtual);   
+Console.WriteLine("Serão " + data5Dias);   
+//Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));   
