2e7c26677bfab1685bca0037987ecca6be13b46f
jonataferreira <ferreira.jonata@gmail.com>
2024-08-05 10:49:46 -0300

    Commit feito após a implementação da função calculadora e tabuada ao código

--- Aula02/Program.cs
+++ Aula02/Program.cs
@@ -2,20 +2,50 @@
 //Console.WriteLine("Hello, World!");
 using Aula02.Models;
 
-Pessoa pessoa= new Pessoa();
+Pessoa pessoa = new Pessoa();
+bool exibirMenu = true;
 
 //string char = Console.ReadLine("Você gostaria de continuar? (S / N): ");
+Console.Clear();
 Console.WriteLine("Você gostaria de continuar? (S / N): ");
 char resposta = Console.ReadLine().ToUpper()[0];
- if (resposta == 'S')
- {
 
-    Console.WriteLine("Digite o número que você gostaria de saber a tabuada: ");
-    int x = Convert.ToInt32(Console.ReadLine());
-    pessoa.Tabuada(x);
-}
+do
+{
+    if (resposta == 'S')
+    {
+        //Console.Clear();
+        Console.WriteLine("Escolha o tipo de ferramenta");
+        Console.WriteLine("1 - Calculadora");
+        Console.WriteLine("2 - Tabuada");
+        Console.WriteLine("Digite Qualquer outra tecla para sair");
+        string opcao = Console.ReadLine();
+     
 
+        switch (opcao)
+        {
+            case "1":
+                pessoa.Calculadora();
+                break;
 
+            case "2":
+                pessoa.Tabuada();
+                break;
+
+            default:
+                exibirMenu = false;
+                break;
+
+        }
+    }
+    else { exibirMenu = false;}
+   
+    
+
+    //pessoa.Tabuada(x);
+} while (exibirMenu == true) ;
+
+Console.WriteLine("Fim da execução");
 
 
 
