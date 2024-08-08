3a0c8a12d7fd088b3ad510f23bb3df68bb1fc81a
jonataferreira <ferreira.jonata@gmail.com>
2024-08-08 10:42:56 -0300

    Commit feito com implementações iniciais sobre exercícios de classes

---
+++ Aula03/Models/Pessoa.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aula03.Models
+{
+    public class Pessoa
+    {
+        public Pessoa(string nome, string sobrenome)
+        {
+
+        }
+        private string _nome;
+        private int _idade; 
+
+        
+        public string Nome 
+        {
+             get => _nome;
+              
+             set 
+             {
+                if (value == "")
+                {
+                    throw new ArgumentException("O nome não pode ser vazio");
+                }
+
+                _nome = value;
+             }
+             
+        }
+        
+        public int Idade 
+        {
+             get
+             {
+                return _idade;
+             }
+
+             set 
+             {
+                if (value < 0)
+                {
+                    throw new ArgumentException("A idade e não pode ser menor que 0");
+                }
+
+                _idade = value;
+             }
+             
+        }
+
+        public string Sobrenome { get; set; }
+        public string NomeCompleto { get => $"{Nome} {Sobrenome}".ToUpper(); }
+
+        public void Apresentar(string nome, int idade)
+        {
+            this.Nome = nome;
+            this.Idade = idade;
+            Console.WriteLine($"Nome: {NomeCompleto}, Idade: {idade}");
+        }
+
+    }
+
+}
---
+++ Aula03/Program.cs
@@ -0,0 +1,12 @@
+﻿using Aula03.Models;
+
+Pessoa pessoa = new Pessoa();
+
+Console.Clear();
+Console.WriteLine("Digite o nome:");
+string nome = (Console.ReadLine());
+Console.WriteLine("Digite a idade:");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+pessoa.Apresentar(nome, idade);
+
